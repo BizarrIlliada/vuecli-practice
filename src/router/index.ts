2b6e2ce946e1fb01c7c162076afe85883c65c782
BizarrIlliada <kazkoviy@icloud.com>
2023-07-20 22:49:57 +0300

    third page was adeed

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,7 +14,15 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/third-page',
+    name: 'third-page',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "third-page" */ '../views/ThirdPageView.vue')
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/ThirdPageView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="third-page">
+    <h1>
+      This is a third page of this app.
+    </h1>
+  </div>
+</template>
